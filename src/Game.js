eb228e3038970c026f7ed827c521297946af724b
Chris Jang <chris3jang@gmail.com>
2017-03-26 21:07:31 -0400

    made win condition functional for full board

--- src/Game.js
+++ src/Game.js
@@ -15,42 +15,74 @@ class Game extends Component {
       [ " ", " ", " ", " ", " ", " ", " ", " ", " " ],
       [ " ", " ", " ", " ", " ", " ", " ", " ", " " ],
       [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
-    ]
+    ],
+    outerboard: [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
     
 	};
 
 	handleMove(indexclicked) { //indexclicked is whatever is fed to position attribute in <Square/>
 		const {turn, boardpositions} = this.state //these variables are undefined until this line, figure out why i put this line in this method and never in the constructor
 		boardpositions[indexclicked[0]][indexclicked[1]] = turn ? 'X' : 'O'; //this line causes <Square/>'s content attribute to change immediately
-		this.setState({turn: !turn, boardpositions}, this.didWin()); //these variables don't get updated until after this function is complete(they will be changed once this.state at the beginning of this function is called again), boardpositions same as "boardpostions: boardposition"
-	};
+		this.setState({turn: !turn, boardpositions}, this.didWin(indexclicked)); //these variables don't get updated until after this function is complete(they will be changed once this.state at the beginning of this function is called again), boardpositions same as "boardpostions: boardposition"
+  };
 
-  	didWin() {
-	    const {turn, boardpositions} = this.state;
-	    const winConditions = [
-	      [0, 1, 2],
-	      [3, 4, 5],
-	      [6, 7, 8],
-	      [0, 3, 6],
-	      [1, 4, 7],
-	      [2, 5, 8],
-	      [0, 4, 8],
-	      [2, 4, 6],
-	    ];
-    var winner = ""
-    if(turn) winner = "X"
-    else winner = "O"
-    for(const i=0; i<winConditions.length; i++) {
-      if(boardpositions[winConditions[i][0]] === boardpositions[winConditions[i][1]] &&
-        boardpositions[winConditions[i][1]] === boardpositions[winConditions[i][2]] &&
-        boardpositions[winConditions[i][0]] != " ") {
-        console.log(winner + " player won")
-      }
+
+
+  didWin(indexclicked) {
+    const {turn, boardpositions, outerboard} = this.state;
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    //check for win in current box
+    for(var i=0; i<8; i++) {
+      if(boardpositions[indexclicked[0]][winConditions[i][0]] 
+        === boardpositions[indexclicked[0]][winConditions[i][1]] 
+        && boardpositions[indexclicked[0]][winConditions[i][1]] 
+        === boardpositions[indexclicked[0]][winConditions[i][2]] 
+        && boardpositions[indexclicked[0]][winConditions[i][1]] 
+        !== " ") {
+          console.log("won an inner box");
+          outerboard[indexclicked[0]] = turn ? 'X' : '0';
+          this.setState(outerboard, this.didWinOuter(indexclicked[0]))
+          break;
+        }
+    }
+  }
+
+  //check for win on outer board only when an inner box is won
+  didWinOuter(outerindexclicked) {
+    const {outerboard} = this.state
+    const winconditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for(var i=0; i<8; i++) {
+      if(outerboard[winconditions[i][0]] === outerboard[winconditions[i][1]] &&
+        outerboard[winconditions[i][1]] === outerboard[winconditions[i][2]] &&
+        outerboard[winconditions[i][0]] !== " ") {
+          console.log("won an outerbox/the game")
+        }
     }
   }
 
   handleMove = this.handleMove.bind(this);
   didWin = this.didWin.bind(this);
+  //didWinInner = this.didWinInner.bind(this);
+  didWinOuter = this.didWinOuter.bind(this);
 
   render() {
   	const {boardpositions} = this.state
